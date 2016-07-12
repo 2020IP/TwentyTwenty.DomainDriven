b4557400f11beae2706044ca2b8dad014183034e
Eric Green <edgeric@gmail.com>
2016-07-12 13:46:29 -0500

    Removed event publisher send for now

--- src/TwentyTwenty.DomainDriven/IEventPublisher.cs
+++ src/TwentyTwenty.DomainDriven/IEventPublisher.cs
@@ -5,7 +5,5 @@ namespace TwentyTwenty.DomainDriven
 	public interface IEventPublisher
 	{
 	    Task Publish<T>(T @event) where T : class, IDomainEvent;
-
-        Task Send<T>(T @event) where T : class, IDomainEvent;
     }
 }
