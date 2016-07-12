9c6d5c4a6514c942ece61c3c299b1e468ec1bb97
Eric Green <edgeric@gmail.com>
2016-07-12 08:17:52 -0500

    Added event publishing abstractions

---
+++ src/TwentyTwenty.DomainDriven/EventPublishing/EventPublishingAggregateRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyTwenty.DomainDriven.EventPublishing
+{
+    public abstract class EventPublishingAggregateRoot<TAggregate> : Entity<Guid>, IAggregateRoot<Guid>
+        where TAggregate : EventPublishingAggregateRoot<TAggregate>
+    {
+        private readonly List<IDomainEvent> _changes = new List<IDomainEvent>();
+
+        public IEnumerable<IDomainEvent> GetUnpublishedEvents()
+        {
+            return _changes;
+        }
+
+        public void MarkEventsAsPublished()
+        {
+            _changes.Clear();
+        }
+
+        protected void AddEvent(IDomainEvent @event)
+        {
+            _changes.Add(@event);
+        }
+    }
+}
---
+++ src/TwentyTwenty.DomainDriven/EventPublishing/IEventPublishingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TwentyTwenty.DomainDriven.EventPublishing
+{
+    public interface IEventPublishingRepository
+    {
+        T Save<T>(T entity) where T : EventPublishingAggregateRoot<T>, new();
+
+        Task<T> SaveAsync<T>(T entity) where T : EventPublishingAggregateRoot<T>, new();
+
+        T GetById<T>(Guid id) where T : EventPublishingAggregateRoot<T>, new();
+
+        Task<T> GetByIdAsync<T>(Guid id) where T : EventPublishingAggregateRoot<T>, new();
+    }
+}
