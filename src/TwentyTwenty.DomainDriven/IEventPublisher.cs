6bc0855e18d9ccf63b0cc8d187b9040af3afc49d
Eric Green <edgeric@gmail.com>
2016-07-12 13:07:45 -0500

    added send to ieventpublisher

--- src/TwentyTwenty.DomainDriven/IEventPublisher.cs
+++ src/TwentyTwenty.DomainDriven/IEventPublisher.cs
@@ -5,5 +5,7 @@ namespace TwentyTwenty.DomainDriven
 	public interface IEventPublisher
 	{
 	    Task Publish<T>(T @event) where T : class, IDomainEvent;
+
+        Task Send<T>(T @event) where T : class, IDomainEvent;
     }
 }
