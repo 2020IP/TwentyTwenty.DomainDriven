7895f544eb82d3cf7b4485f21326b56250bd45f7
Jarrod Alexander <jarrod.alexander@gmail.com>
2017-06-24 10:17:13 -0500

    Adding request-response support to in-proc bus.

--- src/TwentyTwenty.DomainDriven/CQRS/ICommandSender.cs
+++ src/TwentyTwenty.DomainDriven/CQRS/ICommandSender.cs
@@ -6,14 +6,14 @@ namespace TwentyTwenty.DomainDriven.CQRS
     public interface ICommandSender
     {
         Task Send(ICommand command);
-
         Task Send(ICommand command, Type commandType);
     }
 
     public interface ICommandSenderReceiver
     {
-        Task<TResult> Send<TResult>(ICommand command) where TResult : class, IResponse;
-
-        Task<TResult> Send<TResult>(ICommand command, Type commandType) where TResult : class, IResponse;
+        Task<TResult> Send<TResult>(ICommand command) 
+            where TResult : class, IResponse;
+        Task<TResult> Send<TResult>(ICommand command, Type commandType) 
+            where TResult : class, IResponse;
     }
 }
--- src/TwentyTwenty.DomainDriven/CQRS/InProcessBus.cs
+++ src/TwentyTwenty.DomainDriven/CQRS/InProcessBus.cs
@@ -4,9 +4,15 @@ using System.Threading.Tasks;
 
 namespace TwentyTwenty.DomainDriven.CQRS
 {
-    public class InProcessBus : ICommandSender, IEventPublisher, IHandlerRegistrar
+    public class InProcessBus : 
+        ICommandSender, 
+        ICommandSenderReceiver, 
+        IEventPublisher, 
+        IHandlerRegistrar,
+        IHandlerRequestResponseRegistrar
     {
         private readonly Dictionary<Type, List<Action<IMessage>>> _routes = new Dictionary<Type, List<Action<IMessage>>>();
+        private readonly Dictionary<Type, List<Func<IMessage, Task<object>>>> _responseRoutes = new Dictionary<Type, List<Func<IMessage, Task<object>>>>();
 
         public void RegisterHandler<T>(Action<T> handler) where T : class, IMessage
         {
@@ -16,7 +22,20 @@ namespace TwentyTwenty.DomainDriven.CQRS
                 handlers = new List<Action<IMessage>>();
                 _routes.Add(typeof(T), handlers);
             }
-            handlers.Add((x => handler((T)x)));
+            handlers.Add(x => handler((T)x));
+        }
+
+        public void RegisterHandler<T, TResult>(Func<T, Task<TResult>> handler)
+            where T : class, IMessage
+            where TResult : class, IResponse
+        {
+            List<Func<IMessage, Task<object>>> handlers;
+            if (!_responseRoutes.TryGetValue(typeof(T), out handlers))
+            {
+                handlers = new List<Func<IMessage, Task<object>>>();
+                _responseRoutes.Add(typeof(T), handlers);
+            }
+            handlers.Add(x => handler((T)x).ContinueWith(r => (object)r.Result));
         }
 
         public Task Send(ICommand command)
@@ -37,6 +56,25 @@ namespace TwentyTwenty.DomainDriven.CQRS
             {
                 throw new InvalidOperationException("No handler registered");
             }
+        }
+
+        public Task<TResult> Send<TResult>(ICommand command)
+            where TResult : class, IResponse
+            => Send<TResult>(command, command.GetType());
+
+        public Task<TResult> Send<TResult>(ICommand command, Type commandType)
+            where TResult : class, IResponse
+        {
+            List<Func<IMessage, Task<object>>> handlers;
+            if (_responseRoutes.TryGetValue(commandType, out handlers))
+            {
+                if (handlers.Count != 1)
+                    throw new InvalidOperationException("Cannot send to more than one handler");
+                
+                return handlers[0](command).ContinueWith(r => (TResult)r.Result);
+            }
+            
+            throw new InvalidOperationException("No handler registered");
         }
 
         public Task Publish(IDomainEvent @event)
