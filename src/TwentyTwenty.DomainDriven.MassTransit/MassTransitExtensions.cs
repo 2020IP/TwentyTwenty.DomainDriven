9290f00b4cb7555d6475dd23c2432f9c7f43cb9b
Eric Green <edgeric@gmail.com>
2019-05-23 13:31:20 -0500

    more MT changes

--- src/TwentyTwenty.DomainDriven.MassTransit/MassTransitExtensions.cs
+++ src/TwentyTwenty.DomainDriven.MassTransit/MassTransitExtensions.cs
@@ -5,7 +5,6 @@ using MassTransit;
 using MassTransit.ExtensionsDependencyInjectionIntegration;
 using MassTransit.RabbitMqTransport;
 using MassTransit.Util;
-using Microsoft.Extensions.DependencyInjection;
 using TwentyTwenty.DomainDriven.CQRS;
 
 namespace TwentyTwenty.DomainDriven.MassTransit
@@ -19,15 +18,9 @@ namespace TwentyTwenty.DomainDriven.MassTransit
         }
 
         public static void AddConsumers(this IServiceCollectionConfigurator opt, params Type[] markerTypes)
-            => AddConsumers(opt, false, null, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
+            => AddConsumers(opt, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
 
-            public static void AddConsumers(this IServiceCollectionConfigurator opt, params Assembly[] assemblies)
-            => AddConsumers(opt, false, null, assemblies);
-
-        public static void AddConsumers(this IServiceCollectionConfigurator opt, bool mapDefaultEndpoints, string rabbitMqUri, params Type[] markerTypes)
-            => AddConsumers(opt, mapDefaultEndpoints, rabbitMqUri, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
-
-        public static void AddConsumers(this IServiceCollectionConfigurator opt, bool mapDefaultEndpoints, string rabbitMqUri, params Assembly[] assemblies)
+        public static void AddConsumers(this IRegistrationConfigurator opt, params Assembly[] assemblies)
         {
             var types = AssemblyTypeCache.FindTypes(assemblies, t =>
             {
@@ -37,49 +30,33 @@ namespace TwentyTwenty.DomainDriven.MassTransit
 
             foreach (var type in types)
             {
-                if (mapDefaultEndpoints && !string.IsNullOrEmpty(rabbitMqUri))
+                opt.AddConsumer(type);
+            }
+        }
+
+        public static void MapEndpointConventions(this IRabbitMqBusFactoryConfigurator cfg, string rabbitMqUri, params Type[] markerTypes)
+            => MapEndpointConventions(cfg, rabbitMqUri, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
+
+        public static void MapEndpointConventions(this IRabbitMqBusFactoryConfigurator cfg, string rabbitMqUri, params Assembly[] assemblies)
+        {
+            var types = AssemblyTypeCache.FindTypes(assemblies, t =>
+            {
+                var info = t.GetTypeInfo();
+                return !info.IsAbstract && !info.IsInterface && typeof(IConsumer).IsAssignableFrom(t);
+            }).Result.AllTypes();
+
+            var mapMethod = typeof(EndpointConvention)
+                .GetMethod("Map", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(Uri) }, null);
+
+            foreach (var type in types)
+            {
+                var messageType = type.GetMessageType();
+                if (typeof(ICommand).IsAssignableFrom(messageType))
                 {
-                    var messageType = type.GetMessageType();
-                    var method = typeof(EndpointConvention)
-                        .MakeGenericType(messageType)
-                        .GetMethod("Map", BindingFlags.Static | BindingFlags.Public);
-                    method.Invoke(null, new object[] { null, new Uri($"{rabbitMqUri}/{nameof(messageType)}") });
+                    var generic = mapMethod.MakeGenericMethod(messageType);
+                    generic.Invoke(null, new object[] { new Uri($"{rabbitMqUri}/{messageType.Name}") });
                 }
-
-                opt.InvokeGeneric("AddConsumer", new[] { type });
             }
-        }
-        
-        public static IRabbitMqBusFactoryConfigurator AddEventReceiveEndpoints(this IRabbitMqBusFactoryConfigurator configurator, IRabbitMqHost host, IServiceProvider services)
-        {
-            var cache = services.GetRequiredService<IConsumerCacheService>();
-
-            var eventHandlers = cache.GetConfigurators()
-                .Select(c => new { HandlerType = c.GetType().GetGenericArguments().First(), Configurator = c, })
-                .Where(c => !typeof(ICommand).IsAssignableFrom(c.HandlerType.GetMessageType()));
-
-            foreach (var handler in eventHandlers)
-            {
-                configurator.ReceiveEndpoint(host, handler.HandlerType.Name, c => handler.Configurator.Configure(c, services));
-            }
-
-            return configurator;
-        }
-
-        public static IRabbitMqBusFactoryConfigurator AddCommandReceiveEndpoints(this IRabbitMqBusFactoryConfigurator configurator, IRabbitMqHost host, IServiceProvider services)
-        {
-            var cache = services.GetRequiredService<IConsumerCacheService>();
-
-            var commandHandlers = cache.GetConfigurators()
-                .Select(c => new { MessageType = c.GetType().GetGenericArguments().First().GetMessageType(), Configurator = c, })
-                .Where(c => typeof(ICommand).IsAssignableFrom(c.MessageType));
-
-            foreach (var handler in commandHandlers)
-            {
-                configurator.ReceiveEndpoint(host, handler.MessageType.Name, c => handler.Configurator.Configure(c, services));
-            }
-
-            return configurator;
         }
 
         private static Type FindInterfaceThatCloses(this Type type, Type openType)
