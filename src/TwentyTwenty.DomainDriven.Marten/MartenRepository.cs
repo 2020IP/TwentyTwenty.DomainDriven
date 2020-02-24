9f6a7c0a4a96447ab38a5dfc11cfe8696a323fe5
Eric Green <edgeric@gmail.com>
2020-02-24 09:37:10 -0600

    Update to latest MassTransit

--- src/TwentyTwenty.DomainDriven.Marten/MartenRepository.cs
+++ src/TwentyTwenty.DomainDriven.Marten/MartenRepository.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Marten;
+using Marten.Util;
 using TwentyTwenty.DomainDriven.EventSourcing;
 
 namespace TwentyTwenty.DomainDriven.Marten
