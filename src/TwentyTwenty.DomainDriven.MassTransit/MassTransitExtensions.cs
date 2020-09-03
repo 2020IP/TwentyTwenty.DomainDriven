e33ab035db5a63429d3d0421e28c42671d99ccc2
Eric Green <edgeric@gmail.com>
2020-09-03 07:25:21 -0500

    Generic MapEndpointConventions

--- src/TwentyTwenty.DomainDriven.MassTransit/MassTransitExtensions.cs
+++ src/TwentyTwenty.DomainDriven.MassTransit/MassTransitExtensions.cs
@@ -2,8 +2,6 @@ using System;
 using System.Linq;
 using System.Reflection;
 using MassTransit;
-using MassTransit.ActiveMqTransport;
-using MassTransit.RabbitMqTransport;
 using MassTransit.Util;
 using TwentyTwenty.DomainDriven.CQRS;
 
@@ -11,10 +9,10 @@ namespace TwentyTwenty.DomainDriven.MassTransit
 {
     public static class MassTransitExtensions
     {
-        public static void MapEndpointConventions(this IActiveMqBusFactoryConfigurator cfg, string rabbitMqUri, params Type[] markerTypes)
-            => MapEndpointConventions(cfg, rabbitMqUri, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
+        public static void MapEndpointConventions(this IBusFactoryConfigurator cfg, string messageUri, params Type[] markerTypes)
+            => MapEndpointConventions(cfg, messageUri, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
 
-        public static void MapEndpointConventions(this IActiveMqBusFactoryConfigurator cfg, string rabbitMqUri, params Assembly[] assemblies)
+        public static void MapEndpointConventions(this IBusFactoryConfigurator cfg, string messageUri, params Assembly[] assemblies)
         {
             var types = AssemblyTypeCache.FindTypes(assemblies, t =>
             {
@@ -28,28 +26,7 @@ namespace TwentyTwenty.DomainDriven.MassTransit
             foreach (var type in types)
             {
                 var generic = mapMethod.MakeGenericMethod(type);
-                generic.Invoke(null, new object[] { new Uri($"{rabbitMqUri}/{type.Name}") });
-            }
-        }
-
-        public static void MapEndpointConventions(this IRabbitMqBusFactoryConfigurator cfg, string rabbitMqUri, params Type[] markerTypes)
-            => MapEndpointConventions(cfg, rabbitMqUri, markerTypes.Select(t => t.GetTypeInfo().Assembly).ToArray());
-
-        public static void MapEndpointConventions(this IRabbitMqBusFactoryConfigurator cfg, string rabbitMqUri, params Assembly[] assemblies)
-        {
-            var types = AssemblyTypeCache.FindTypes(assemblies, t =>
-            {
-                var info = t.GetTypeInfo();
-                return !info.IsAbstract && !info.IsInterface && typeof(ICommand).IsAssignableFrom(t);
-            }).Result.AllTypes();
-
-            var mapMethod = typeof(EndpointConvention)
-                .GetMethod("Map", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(Uri) }, null);
-
-            foreach (var type in types)
-            {
-                var generic = mapMethod.MakeGenericMethod(type);
-                generic.Invoke(null, new object[] { new Uri($"{rabbitMqUri}/{type.Name}") });
+                generic.Invoke(null, new object[] { new Uri($"{messageUri}/{type.Name}") });
             }
         }
 
