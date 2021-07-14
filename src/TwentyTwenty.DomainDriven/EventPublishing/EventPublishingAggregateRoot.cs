776423f1aa730e0a86f6d1a6a368749190179eb2
Eric Green <edgeric@gmail.com>
2021-07-14 12:19:40 -0500

    IgnoreDataMember HasUnpublishedEvents

--- src/TwentyTwenty.DomainDriven/EventPublishing/EventPublishingAggregateRoot.cs
+++ src/TwentyTwenty.DomainDriven/EventPublishing/EventPublishingAggregateRoot.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace TwentyTwenty.DomainDriven.EventPublishing
 {
     public abstract class EventPublishingAggregateRoot<TId> : Entity<TId>, IEventPublishingAggregateRoot<TId>
     {
         protected readonly List<IDomainEvent> _changes = new List<IDomainEvent>();
-
-        public virtual IEnumerable<IDomainEvent> GetUnpublishedEvents() => _changes;
-
-        public virtual bool HasUnpublishedEvents => _changes.Count > 0;
-
-        public virtual void MarkEventsAsPublished() => _changes.Clear();
-
         protected virtual void AddEvent(IDomainEvent @event) => _changes.Add(@event);
+        
+        [IgnoreDataMember]
+        public virtual bool HasUnpublishedEvents => _changes.Count > 0;
+        public virtual IEnumerable<IDomainEvent> GetUnpublishedEvents() => _changes;
+        public virtual void MarkEventsAsPublished() => _changes.Clear();
     }
 }
