9ebfd4299b4e47a9caac8082acea70fddf78a175
Jamie Johns <jamie.johns@sago.com>
2026-01-15 16:45:13 -0600

    adding logging for build error

--- build.ps1
+++ build.ps1
@@ -9,5 +9,11 @@ $env:DOTNET_NOLOGO = '1'
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet cake @args
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+try {
+    dotnet cake build.cake --target=Version --verbosity=Diagnostic
+}
+catch {
+    Write-Host "GitVersion / Version step failed:"
+    Write-Host $_.Exception.ToString()
+    throw
+}
